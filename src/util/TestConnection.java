caa0fc41696fd11a2affac601a4a0ff5b1b5a232
Kashish811 <kashish081106@gmail.com>
2026-05-02 01:38:58 +0530

    Remove comments from TestConnection class
    Removed class-level comments from TestConnection.

--- src/util/TestConnection.java
+++ src/util/TestConnection.java
@@ -1,9 +1,5 @@
 package util;
 
-/**
- * Class TestConnection
- * Simple comment for TestConnection
- */
 public class TestConnection {
         public static void main(String[] args) {
         System.out.println("TestConnection disabled due to compilation errors interfering with builds.");
