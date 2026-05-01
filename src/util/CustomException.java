da9fe5625f7e57149d1e410f3dda535dbfa2bcc6
Kashish811 <kashish081106@gmail.com>
2026-05-02 01:38:11 +0530

    Clean up comments in CustomException.java
    Removed unnecessary comments from CustomException class

--- src/util/CustomException.java
+++ src/util/CustomException.java
@@ -1,9 +1,5 @@
 package util;
 
-/**
- * Class CustomException
- * Simple comment for CustomException
- */
 public class CustomException extends Exception {
     
         public CustomException(String message) {
